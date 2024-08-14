d917525bdbbdeaec05a388a353aeb25e8097f6be
novanota <notawhywhy@gmail.com>
2024-08-14 21:54:02 +0300

    added 'Convert a number to a string' solutions

---
+++ Convert a Number to a String!/ConvertANumberToAString.js
@@ -0,0 +1,11 @@
+const numberToString = (num) => {
+    return String(num);
+};
+
+const numberToStrUsingTemplate = (num) => {
+    return `${num}`;
+};
+
+const convertNumberByConcatenation = (num) => {
+    return num + "";
+};

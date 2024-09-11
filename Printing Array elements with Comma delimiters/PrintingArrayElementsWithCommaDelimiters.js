9fac084584e8226c0778790ded957abce168d485
novanota <notawhywhy@gmail.com>
2024-09-11 15:13:32 +0300

    added 'Printing array elements with comma delimiters' solutions

---
+++ Printing Array elements with Comma delimiters/PrintingArrayElementsWithCommaDelimiters.js
@@ -0,0 +1,11 @@
+const printArray = (array) => {
+    return array.join(",");
+};
+
+const toStringArray = (array) => {
+    return array.toString();
+};
+
+const mapAndJoin = (array) => {
+    return array.map(String).join(",");
+};

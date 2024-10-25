72a5523ce5c3cf75c43c00de930058efa77afcf1
novanota <notawhywhy@gmail.com>
2024-10-25 16:45:49 +0300

    added 'My head is at the wrong end' solutions

---
+++ ⚪ 8 - kyu/My head is at the wrong end!/MyHeadIsAtTheWrongEnd.js
@@ -0,0 +1,5 @@
+const fixTheMeerkat = (arr) => arr.reverse();
+
+const fixAnimalOrientation = (arr) => [arr[2], arr[1], arr[0]];
+
+const restoreMeerkat = ([tail, body, head]) => [head, body, tail];

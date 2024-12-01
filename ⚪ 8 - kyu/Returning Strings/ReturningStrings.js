187bca6a187fcf3a8ca83015696420c8dfcb3215
boredboro <notawhywhy@gmail.com>
2024-12-01 15:58:01 +0200

    added 'Returning Strings' solutions

---
+++ ⚪ 8 - kyu/Returning Strings/ReturningStrings.js
@@ -0,0 +1,6 @@
+const greet = (name) => `Hello, ${name} how are you doing today?`;
+
+const greetConcat = (name) => "Hello, " + name + " how are you doing today?";
+
+const greetMethod = (name) =>
+    "Hello, ".concat(name, " how are you doing today?");
